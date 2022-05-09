6cbed2a9a1e99cf4e7e1fdcea313abce91aa2b9e
mileAlejandre <ada_hack166@hackademy.mx>
2022-05-08 22:57:26 -0500

    JavaScript 2

---
+++ JavaScript2/arreglo.js
@@ -0,0 +1,16 @@
+let ar =[3, 4, 5, 6, 2, 3];
+let total=0;
+
+
+function sumaArreglo(){
+for(let i of ar) total+=i;
+console.log(total);
+}
+sumaArreglo();
+
+const salad = ['🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑'];
+
+for(let i=0; i<salad.length; i++) {
+  console.log(`Element at index ${i} is ${salad[i]}`);
+}
+
